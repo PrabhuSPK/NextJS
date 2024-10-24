85ef8ecfeebb11cb3d66ce3beaa471c3bdbb7145
Prabhu Selvamani <prabhuselvamani1995@gmail.com>
2024-10-24 21:26:07 +0530

    Next.js App Fonts- Multiple Single Font Usage

---
+++ prabhu-app/src/app/fonts.js
@@ -0,0 +1,14 @@
+import {Kablammo, Danfo} from "next/font/google";
+
+
+export const kablammo = Kablammo({
+    subsets : ["latin"],
+    display : "swap",
+    weight  : ["400"]
+});
+
+export const danfo = Danfo({
+    subsets : ["latin"],
+    display : "swap",
+    weight  : ["400"]
+});
--- prabhu-app/src/app/page.js
+++ prabhu-app/src/app/page.js
@@ -2,11 +2,14 @@ import Image from "next/image";
 import styles from "./page.module.css";
 import About from "@/components/About/About";
 import Contact from "@/components/Contact/Contact";
+import { danfo, kablammo } from "./fonts";
 
 export default function Home() {
   return (
     <div className={styles.page}>
-      <h1>Components Usage</h1>
+      <h1>Global Multiple Font Usage</h1>
+      <p className={kablammo.className}>Kablammo Font</p>
+      <p className={danfo.className}>Danfo Font</p>
       <About />
       <Contact />
     </div>
