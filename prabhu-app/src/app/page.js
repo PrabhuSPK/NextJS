5971a4946cabf5b588c920781bc9c8c409dddfc3
Prabhu Selvamani <prabhuselvamani1995@gmail.com>
2024-10-24 21:17:13 +0530

    Next.js App - Components Usage

--- prabhu-app/src/app/page.js
+++ prabhu-app/src/app/page.js
@@ -1,10 +1,14 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import About from "@/components/About/About";
+import Contact from "@/components/Contact/Contact";
 
 export default function Home() {
   return (
     <div className={styles.page}>
-      <h1>Global Single Font Usage</h1>
+      <h1>Components Usage</h1>
+      <About />
+      <Contact />
     </div>
   );
 }
---
+++ prabhu-app/src/components/About/About.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div>About Components</div>
+  )
+}
+
+export default About
