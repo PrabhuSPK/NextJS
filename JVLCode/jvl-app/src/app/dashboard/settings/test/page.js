e6a77816cf6dfccde7c76c11e4bebcb60e1dc5f9
Prabhu Selvamani <prabhuselvamani1995@gmail.com>
2024-12-20 20:42:45 +0530

    Layouts & Props - Nested Routing - useRouter (hook) for back button - No Reload

--- JVLCode/jvl-app/src/app/dashboard/settings/test/page.js
+++ JVLCode/jvl-app/src/app/dashboard/settings/test/page.js
@@ -1,9 +1,21 @@
+"use client"
+import { useRouter } from 'next/navigation'
 import React from 'react'
 
+
+
 const Testpage = () => {
+
+  const router = useRouter();
+
+  const back = () =>{
+    router.push('/dashboard/settings')
+  }
+
   return (
     <div>
     <h1>Test page..</h1>
+    <button className='bg-red-500 text-white px-4 py-2' onClick={back}>Back</button>
     </div>
   )
 }
